aaa8b6134d6d25dc8da205e3c09add67ebdbe90c
FernandaRon <rondon.fernanda11@gmail.com>
2019-04-08 18:12:10 -0500

    first app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component} from "react";
+import {hot} from "react-hot-loader"
 import "./App.css";
+import Uno from "./components/UNO.js"
 
 class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      list : ["Egon Schiele", "Vicent Van G", "Salvador Dali", "Rembrandt"] 
+      boolean: true,
+      str : "hello"
+    }
+  }
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <h1> Hello, Worldiñi </h1>
+        <Uno />
       </div>
     );
   }
 }
-
-export default App;
+export default hot(module)(App);
---
+++ src/components/UNO.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Uno = (props) => (
+  <div>
+   <p>am i a component?</p>
+  </div>
+
+)
+
+export default Uno
